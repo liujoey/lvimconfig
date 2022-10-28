4873a635721f2f6916655d5795cfdec9b1997534
Joey Liu <liu.joey@gmail.com>
2022-10-27 23:21:13 -0400

    fix java debug

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -40,26 +40,14 @@ local workspace_dir = WORKSPACE_PATH .. project_name
 -- git clone git@github.com:microsoft/java-debug.git ~/.config/lvim/.java-debug
 -- cd ~/.config/lvim/.java-debug/
 -- ./mvnw clean install
-local bundles = vim.fn.glob(
-  home .. "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"
-)
-if #bundles == 0 then
-  bundles = vim.fn.glob(
-    home .. "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-    1,
-    1
-  )
-end
+local bundles = vim.fn.glob( home .. "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true, true)
 
 -- NOTE: for testing
 -- git clone git@github.com:microsoft/vscode-java-test.git ~/.config/lvim/.vscode-java-test
 -- cd ~/.config/lvim/vscode-java-test
 -- npm install
 -- npm run build-plugin
-local extra_bundles = vim.split(vim.fn.glob(home .. "/.local/share/nvim/mason/packages/java-test/extension/server/*.jar", 1), "\n")
-if #extra_bundles == 0 then
-  extra_bundles = vim.fn.glob(home .. "/.local/share/nvim/mason/packages/java-test/extension/server/*.jar", 1, 1)
-end
+local extra_bundles = vim.fn.glob(home .. "/.local/share/nvim/mason/packages/java-test/extension/server/*.jar", true, true)
 vim.list_extend(bundles, extra_bundles)
 
 local config = {
